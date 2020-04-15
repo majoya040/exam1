ba316b048284b7a320cb2f5ccd7c28e4356ebf46
majoya040 <34689250+majoya040@users.noreply.github.com>
2020-04-15 23:55:39 +0600

    exam1

---
+++ assets/js/custom.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:true,
+    autoplay:true,
+    autoplayTimeout: 8000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:1
+        }
+    }
+})
